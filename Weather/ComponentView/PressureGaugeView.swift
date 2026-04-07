3b748669d987862608e8230fab2213086c5c97a4
Tri Vo <warbo.trivo@gmail.com>
2026-04-08 04:31:02 +0700

    Added feature of storing favorite cities in disk to keep data persistent by using UserDefaults.

---
+++ Weather/ComponentView/PressureGaugeView.swift
@@ -0,0 +1,18 @@
+//
+//  PressureGaugeView .swift
+//  Weather
+//
+//  Created by warbo on 24/12/25.
+//
+
+import SwiftUI
+
+struct PressureGaugeView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PressureGaugeView_()
+}
---
+++ Weather/ComponentView/SolarPathView.swift
@@ -0,0 +1,18 @@
+//
+//  SolarPathView.swift
+//  Weather
+//
+//  Created by warbo on 25/12/25.
+//
+
+import SwiftUI
+
+struct SolarPathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SolarPathView()
+}
